24628bf5ccb23a770e98720e634c0ac3ddd0d1f1
Lifegnard <math.bertoia@gmail.com>
2014-04-18 16:53:25 +0200

    initialisation bootstrap

--- ProjetBERTOIAGAYOUT/grails-app/conf/BootStrap.groovy
+++ ProjetBERTOIAGAYOUT/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,33 @@
+import projetbertoiagayout.Auteur
+import projetbertoiagayout.Livre
+import projetbertoiagayout.Reservation
+import projetbertoiagayout.TypeDocument
+
+
 class BootStrap {
 
     def init = { servletContext ->
+		TypeDocument type1 = new TypeDocument (intitule : "Journal")
+		
+		Auteur aut1 = new Auteur ( nom : "Bertoia" , prenom :"Mathieu")
+		
+		Livre livre1 = new Livre (titre : "La depeche", nombreExemplaire : 3, nombreExemplaireDisponible : 3 ,doc : type1 )
+		Livre livre2 = new Livre (titre : "Metro", nombreExemplaire : 5, nombreExemplaireDisponible : 5 ,doc : type1 )
+		
+		aut1.addToLivre(livre1)
+		aut1.addToLivre(livre2)
+		
+		Reservation r1 = new Reservation ( code : 1 , dateReservation : new Date("18/04/2014"))
+		
+		type1.save()
+		aut1.save()
+		livre1.save()
+		livre2.save()
+		
+		
+		
+		
+		
     }
     def destroy = {
     }
