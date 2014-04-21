75f2fc825dc41dc734ba2b90b4f912d5decae0c9
Lifegnard <math.bertoia@gmail.com>
2014-04-21 18:44:46 +0200

    test classe auteur

--- ProjetBERTOIAGAYOUT/test/unit/projetbertoiagayout/AuteurTests.groovy
+++ ProjetBERTOIAGAYOUT/test/unit/projetbertoiagayout/AuteurTests.groovy
@@ -10,8 +10,22 @@ import org.junit.*
  */
 @TestFor(Auteur)
 class AuteurTests {
-
-    void testSomething() {
-       fail "Implement me"
+	
+	@Test
+    void testCreation() {
+       Auteur a = new Auteur(nom:"Bertoia", prenom:"Mathieu")
+	   assert (a != null)
+	   assert (a.save() != null)
     }
+	@Test
+	void testBlank(){
+		Auteur a = new Auteur(nom:"", prenom:"")
+		assert (a != null)
+		assert (a.save() == null)
+	}
+	@Test
+	void testToString(){
+		Auteur a = new Auteur(nom:"Bertoia",prenom:"Mathieu")
+		assert (a.toString() == "Bertoia Mathieu")
+	}
 }
