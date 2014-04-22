e8d724d722bf3ffc15d5cec0c9e3c5f20672150e
Lifegnard <math.bertoia@gmail.com>
2014-04-22 21:47:04 +0200

    Test des classes Livre, Reservation, TypeDocument

--- ProjetBERTOIAGAYOUT/test/unit/projetbertoiagayout/LivreTests.groovy
+++ ProjetBERTOIAGAYOUT/test/unit/projetbertoiagayout/LivreTests.groovy
@@ -3,15 +3,53 @@ package projetbertoiagayout
 
 
 import grails.test.mixin.*
+
 import org.junit.*
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Livre)
+@Mock([Livre,Reservation])
 class LivreTests {
-
-    void testSomething() {
-       fail "Implement me"
+	
+	@Test
+    void testCreation() {
+		TypeDocument type = new TypeDocument( intitule : "BD" )
+		Livre livre = new Livre( titre : "Tintin", nombreExemplaire : 5, nombreExemplaireDisponible : 5, doc : type )
+		assert (livre != null)
+		assert (livre.save() != null)
     }
+	@Test
+	void testCreationBlank() {
+		TypeDocument type = new TypeDocument( intitule : "BD" )
+		Livre livre = new Livre( titre : "", nombreExemplaire : "", nombreExemplaireDisponible :"",doc : "" )
+
+		assert livre != null
+		assert livre.save() == null
+	}
+	@Test
+	void testToString() {
+		TypeDocument type = new TypeDocument( intitule : "BD" )
+		Livre livre = new Livre( titre : "Tintin", nombreExemplaire : 5, nombreExemplaireDisponible : 5, doc : type )
+
+		assert livre.toString() == "Tintin,5,5"
+		
+	}
+	@Test
+	void testaddReservation() {
+		TypeDocument type = new TypeDocument( intitule : "BD" )
+		Livre livre = new Livre( titre : "Tintin", nombreExemplaire : 5, nombreExemplaireDisponible : 5 , doc : type)
+		assert livre != null
+		assert livre.save() != null
+
+		assert livre.getNombreExemplaireDisponible() == 5
+
+		Reservation res = new Reservation( code : 1, dateReservation : new Date("22/04/2014") )
+		
+
+		livre.addReservation(res)
+		assert livre.getNombreExemplaireDisponible() == 4
+
+	}
 }
--- ProjetBERTOIAGAYOUT/test/unit/projetbertoiagayout/ReservationTests.groovy
+++ ProjetBERTOIAGAYOUT/test/unit/projetbertoiagayout/ReservationTests.groovy
@@ -11,7 +11,22 @@ import org.junit.*
 @TestFor(Reservation)
 class ReservationTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+  void testCreation() {
+		TypeDocument type = new TypeDocument( intitule : "BD" )
+		Livre livre = new Livre( titre : "Tintin", nombreExemplaire : 5, nombreExemplaireDisponible : 5 , doc : type)
+		Reservation res = new Reservation( code : 1, dateReservation : new Date("22/04/2014") )
+		assert res != null
+		assert res.save() != null
+	}
+
+	void testCreationBlank() {
+		Reservation res = new Reservation( code : "", dateReservation: "" )
+		assert res != null
+		assert res.save() == null
+	}
+
+	void testToString() {
+		Reservation res = new Reservation( code : 1, dateReservation : new Date("22/04/2014") )
+		assert res.toString() == "code: 1,"+res.getDateReservation() //"code: "+code+","+dateReservation
+	}
 }
